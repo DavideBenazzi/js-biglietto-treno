4bdb8f065488c7cd29c0af31a0bf4c011f965091
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-14 15:02:22 +0200

    Lavorazione javascript

--- js/main.js
+++ js/main.js
@@ -6,21 +6,38 @@
     va applicato uno sconto del 20% per i minorenni
     va applicato uno sconto del 40% per gli over 65
 */
+
 var km = parseInt( prompt('Quanti chilometri vuoi percorrere?') );
+//// DEBUG:
+console.log(km);
 
 var eta = parseInt( prompt('Quanti anni hai?') );
+//// DEBUG:
+console.log(eta);
+
+//Condizione per validare input
+if ( isNaN(km) || isNaN(eta) ) {
+  alert('Attenzione! I valori inseriti non sono corretti.');
+}
+
 //Prezzo del biglietto per kilometro
 var prezzoKm = 0.21;
+
 //Prezzo scontato del 20% per minorenni
-var prezzoKmMinori = 0.21 * 0.8 ;
+var prezzoKmMinori = Math.trunc(0.21 * 0.8 * 100) / 100 ;
+
 //Prezzo scontato del 40% per over 65 enni
-var prezzoKmOver = 0.21 * 0.6 ;
+var prezzoKmOver = Math.trunc(0.21 * 0.6 * 100) / 100 ;
+
 //Calcolo per biglietto
 var biglietto = km * prezzoKm;
+
 //Calcolo per biglietto minorenni
 var bigliettoMinori = km * prezzoKmMinori ;
+
 //Calcolo per biglietto over 65
 var bigliettoOver = km * prezzoKmOver ;
+
 //Condizione per Calcolare l'eta' per lo sconto
 if (eta > 65) {
   document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ bigliettoOver;
