706e87406051ab4aecab99dc71a0da18612b7b11
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-14 16:15:29 +0200

    Modifiche finali

--- js/main.js
+++ js/main.js
@@ -8,26 +8,25 @@
 */
 
 var km = parseInt( prompt('Quanti chilometri vuoi percorrere?') );
-//// DEBUG:
-console.log(km);
+//Condizione per validare input
+if ( isNaN(km) ) {
+  alert('Attenzione! Il valore inserito non è un numero.');
+}
 
 var eta = parseInt( prompt('Quanti anni hai?') );
-//// DEBUG:
-console.log(eta);
-
 //Condizione per validare input
-if ( isNaN(km) || isNaN(eta) ) {
-  alert('Attenzione! I valori inseriti non sono corretti.');
+if ( isNaN(eta) ) {
+  alert('Attenzione! Il valore inserito non è un numero.');
 }
 
 //Prezzo del biglietto per kilometro
 var prezzoKm = 0.21;
 
 //Prezzo scontato del 20% per minorenni
-var prezzoKmMinori = 0.21 * 0.8;
+var prezzoKmMinori = prezzoKm * 0.8;
 
 //Prezzo scontato del 40% per over 65 enni
-var prezzoKmOver = 0.21 * 0.6;
+var prezzoKmOver = prezzoKm * 0.6;
 
 //Calcolo per biglietto
 var biglietto = (km * prezzoKm).toFixed(2);
@@ -39,12 +38,15 @@ var bigliettoMinori = (km * prezzoKmMinori).toFixed(2);
 var bigliettoOver = (km * prezzoKmOver).toFixed(2);
 
 //Condizione per Calcolare l'eta' per lo sconto
-if (eta > 65) {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ bigliettoOver;
+if ( isNaN(km) || isNaN(eta) ) {
+  document.getElementById('prezzo').innerHTML = 'Per cortesia ricaricare la pagina ed immettere valori coerenti, grazie per la disponibilita\'.';
+}
+else if (eta > 65) {
+  document.getElementById('prezzo').innerHTML = 'Il biglietto è scontato del 40% per i passeggeri over 65 anni, il prezzo è : '+ bigliettoOver + ' €';
 }
 else if (eta < 18) {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ bigliettoMinori;
+  document.getElementById('prezzo').innerHTML = 'Il biglietto è scontato del 20% per i passeggeri minorenni, il prezzo è : '+ bigliettoMinori + ' €';
 }
 else {
-  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ biglietto;
+  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ biglietto + ' €' + ' Buon Viaggio !';
 }
