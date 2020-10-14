bd8f26442648e350cfac9b6d8939f3f6f066615d
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-14 14:37:14 +0200

    Lavorazione logica del costo biglietto

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
-// Controllo integrazione JAVASCRIPT
-console.log('Javascript');
+/*  Chiedere numero di kilometri da percorrere
+    Chiedere eta' passeggero
+
+    Calcolare prezzo totale del viaggio
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65
+*/
+var km = parseInt( prompt('Quanti chilometri vuoi percorrere?') );
+
+var eta = parseInt( prompt('Quanti anni hai?') );
+//Prezzo del biglietto per kilometro
+var prezzoKm = 0.21;
+//Prezzo scontato del 20% per minorenni
+var prezzoKmMinori = 0.21 * 0.8 ;
+//Prezzo scontato del 40% per over 65 enni
+var prezzoKmOver = 0.21 * 0.6 ;
+//Calcolo per biglietto
+var biglietto = km * prezzoKm;
+//Calcolo per biglietto minorenni
+var bigliettoMinori = km * prezzoKmMinori ;
+//Calcolo per biglietto over 65
+var bigliettoOver = km * prezzoKmOver ;
+//Condizione per Calcolare l'eta' per lo sconto
+if (eta > 65) {
+  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ bigliettoOver;
+}
+else if (eta < 18) {
+  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ bigliettoMinori;
+}
+else {
+  document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è : '+ biglietto;
+}
